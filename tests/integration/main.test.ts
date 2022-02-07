0c0ca6b1347b32eae31fe46d122a6793f9dec7dc
Cardinal <cardinalby@gmail.com>
2022-02-07 23:38:07 +0300

    use "github-action-ts-run-api": "^2.0.0"

--- tests/integration/main.test.ts
+++ tests/integration/main.test.ts
@@ -3,8 +3,8 @@ import {RunOptions, RunTarget} from "github-action-ts-run-api";
 describe('main', () => {
     const target = RunTarget.jsFile('lib/index.js', 'action.yml');
 
-    it('should validate action.yml against github-action schema', () => {
-        const res = target.run(RunOptions.create({
+    it('should validate action.yml against github-action schema', async () => {
+        const res = await target.run(RunOptions.create({
             inputs: {
                 file: 'action.yml',
                 schema: 'https://json.schemastore.org/github-action.json'
@@ -15,8 +15,8 @@ describe('main', () => {
         expect(res.commands.outputs).toEqual({});
     });
 
-    it('should validate file by $schema set in file', () => {
-        const res = target.run(RunOptions.create({
+    it('should validate file by $schema set in file', async () => {
+        const res = await target.run(RunOptions.create({
             inputs: {
                 file: 'tests/integration/data/package.schema.json',
             }
@@ -26,8 +26,8 @@ describe('main', () => {
         expect(res.commands.outputs).toEqual({});
     });
 
-    it('should validate action.yml against angular schema', () => {
-        const res = target.run(RunOptions.create({
+    it('should validate action.yml against angular schema', async () => {
+        const res = await target.run(RunOptions.create({
             inputs: {
                 file: 'action.yml',
                 schema: 'https://raw.githubusercontent.com/angular/angular-cli/master/packages/angular/cli/lib/config/workspace-schema.json'
@@ -38,8 +38,8 @@ describe('main', () => {
         expect(res.commands.outputs).toEqual({errorType: 'validation'});
     });
 
-    it('should validate package.json against schema', () => {
-        const res = target.run(RunOptions.create({
+    it('should validate package.json against schema', async () => {
+        const res = await target.run(RunOptions.create({
             inputs: {
                 file: 'package.json',
                 schema: 'tests/integration/data/package.schema.json'
@@ -50,8 +50,8 @@ describe('main', () => {
         expect(res.commands.outputs).toEqual({});
     });
 
-    it('should throw schema error on invalid schema', () => {
-        const res = target.run(RunOptions.create({
+    it('should throw schema error on invalid schema', async () => {
+        const res = await target.run(RunOptions.create({
             inputs: {
                 file: 'package.json',
                 schema: 'LICENSE'
@@ -62,8 +62,8 @@ describe('main', () => {
         expect(res.commands.outputs).toEqual({errorType: 'schema'});
     });
 
-    it('should throw schema error on missing schema', () => {
-        const res = target.run(RunOptions.create({
+    it('should throw schema error on missing schema', async () => {
+        const res = await target.run(RunOptions.create({
             inputs: {
                 file: 'package.json'
             }
@@ -73,8 +73,8 @@ describe('main', () => {
         expect(res.commands.outputs).toEqual({errorType: 'schema'});
     });
 
-    it('should throw remote schema error', () => {
-        const res = target.run(RunOptions.create({
+    it('should throw remote schema error', async () => {
+        const res = await target.run(RunOptions.create({
             inputs: {
                 file: 'package.json',
                 schema: 'https://dwedwoo430930jfgerno9w04.com/'
@@ -85,8 +85,8 @@ describe('main', () => {
         expect(res.commands.outputs).toEqual({errorType: 'schema'});
     });
 
-    it('should throw file error', () => {
-        const res = target.run(RunOptions.create({
+    it('should throw file error', async () => {
+        const res = await target.run(RunOptions.create({
             inputs: {
                 file: 'LICENSE',
                 schema: 'tests/integration/data/package.schema.json'
@@ -97,8 +97,8 @@ describe('main', () => {
         expect(res.commands.outputs).toEqual({errorType: 'file'});
     });
 
-    it('should validate by glob', () => {
-        const res = target.run(RunOptions.create({
+    it('should validate by glob', async () => {
+        const res = await target.run(RunOptions.create({
             inputs: {
                 file: 'tests/integration/data/files/package_*.json',
                 schema: 'tests/integration/data/package.schema.json'
@@ -109,8 +109,8 @@ describe('main', () => {
         expect(res.commands.outputs).toEqual({});
     });
 
-    it('should validate list of files', () => {
-        const res = target.run(RunOptions.create({
+    it('should validate list of files', async () => {
+        const res = await target.run(RunOptions.create({
             inputs: {
                 file: 'tests/integration/data/files/package_*.json|tests/integration/data/files/3_package.json',
                 schema: 'tests/integration/data/package.schema.json'
@@ -121,8 +121,8 @@ describe('main', () => {
         expect(res.commands.outputs).toEqual({});
     });
 
-    it('should validate list of files with invalid', () => {
-        const res = target.run(RunOptions.create({
+    it('should validate list of files with invalid', async () => {
+        const res = await target.run(RunOptions.create({
             inputs: {
                 file: 'tests/integration/data/files/*.json',
                 schema: 'tests/integration/data/package.schema.json'
